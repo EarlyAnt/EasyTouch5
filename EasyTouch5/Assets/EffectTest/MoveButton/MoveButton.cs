78c84f9a3afd8c6d4915e8e430ae3a0c7b8918a4
EarlyAnt <54763755@qq.com>
2020-07-01 12:14:43 +0800

    1.将ETCTouchPad组件放到按钮内部，实现左右滑动时，按钮移动的效果。 2.按钮移动同时，显示和隐藏右侧的图标。

--- EasyTouch5/Assets/EffectTest/MoveButton/MoveButton.cs
+++ EasyTouch5/Assets/EffectTest/MoveButton/MoveButton.cs
@@ -2,44 +2,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MoveButton : MonoBehaviour
 {
     [SerializeField]
     private Transform button;
     [SerializeField]
+    private Image icon;
+    [SerializeField]
     private ETCTouchPad touchPad;
+    [SerializeField, Range(0f, 200f)]
+    private float distance = 100f;
     [SerializeField, Range(0f, 10f)]
     private float valueThreshold = 1f;
     [SerializeField, Range(0f, 10f)]
     private float speedThreshold = 1f;
-    private Tweener tweener;
+    private Sequence sequence;
+    private float timer;
 
     public void SelectButton()
     {
         Debug.LogFormat("<><MoveButton.SelectButton>axisValue: {0}, axisSpeedValue: {1}", this.touchPad.axisX.axisValue, this.touchPad.axisX.axisSpeedValue);
-        // if (Mathf.Abs(this.touchPad.axisX.axisValue) < Mathf.Abs(this.valueThreshold) &&
-        //     Mathf.Abs(this.touchPad.axisX.axisSpeedValue) < Mathf.Abs(this.speedThreshold))
-        //     return;
+
         if (Mathf.Abs(this.touchPad.axisX.axisValue) < Mathf.Abs(this.valueThreshold))
             return;
 
-        if (this.tweener != null)
-            this.tweener.Kill();
-        this.tweener = this.button.DOLocalMoveX(-20f, 0.5f);
+        DOTween.KillAll();
+        this.button.DOLocalMoveX(-this.distance, 0.5f);
+        DOTween.To(() => this.timer, x => this.timer = x, 1f, 0.2f).onComplete += () =>
+        {
+            this.icon.DOFade(1f, 0.3f);
+            this.icon.transform.DOScale(Vector3.one, 0.3f);
+        };
     }
 
     public void UnselecteButton()
     {
         Debug.LogFormat("<><MoveButton.UnselecteButton>axisValue: {0}, axisSpeedValue: {1}", this.touchPad.axisX.axisValue, this.touchPad.axisX.axisSpeedValue);
-        // if (Mathf.Abs(this.touchPad.axisX.axisValue) < Mathf.Abs(this.valueThreshold) &&
-        //     Mathf.Abs(this.touchPad.axisX.axisSpeedValue) < Mathf.Abs(this.speedThreshold))
-        //     return;
+
         if (Mathf.Abs(this.touchPad.axisX.axisValue) < Mathf.Abs(this.valueThreshold))
             return;
 
-        if (this.tweener != null)
-            this.tweener.Kill();
-        this.tweener = this.button.DOLocalMoveX(0f, 0.5f);
+        DOTween.KillAll();
+        this.icon.DOFade(0f, 0.2f);
+        this.icon.transform.DOScale(Vector3.zero, 0.2f);
+        this.button.DOLocalMoveX(0, 0.3f);
     }
 }
